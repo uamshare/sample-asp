c94b759bb98c5efa8e062f5c428a148bdd834851
utiuam <utiuam@gmail.com>
2016-04-05 17:14:44 +0700

    Create Data Transfer Objects (DTOs)

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -19,25 +19,57 @@ namespace BookService.Controllers
         private BookServiceContext db = new BookServiceContext();
 
         // GET api/Books
-        public IQueryable<Book> GetBooks()
+        //public IQueryable<Book> GetBooks()
+        //{
+        //    return db.Books
+        //    // new code:
+        //    .Include(b => b.Author);
+        //}
+
+        public IQueryable<BookDTO> GetBooks()
         {
-            return db.Books
-            // new code:
-            .Include(b => b.Author);
+            var books = from b in db.Books
+                        select new BookDTO()
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            AuthorName = b.Author.Name
+                        };
+
+            return books;
         }
 
         // GET api/Books/5
         [ResponseType(typeof(Book))]
+        //public async Task<IHttpActionResult> GetBook(int id)
+        //{
+        //    Book book = await db.Books.FindAsync(id);
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(book);
+        //}
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books.Include(b => b.Author).Select(b =>
+                new BookDetailDTO()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Year = b.Year,
+                    Price = b.Price,
+                    AuthorName = b.Author.Name,
+                    Genre = b.Genre
+                }).SingleOrDefaultAsync(b => b.Id == id);
             if (book == null)
             {
                 return NotFound();
             }
 
             return Ok(book);
-        }
+        } 
 
         // PUT api/Books/5
         public async Task<IHttpActionResult> PutBook(int id, Book book)
@@ -75,6 +107,18 @@ namespace BookService.Controllers
 
         // POST api/Books
         [ResponseType(typeof(Book))]
+        //public async Task<IHttpActionResult> PostBook(Book book)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Books.Add(book);
+        //    await db.SaveChangesAsync();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+        //}
         public async Task<IHttpActionResult> PostBook(Book book)
         {
             if (!ModelState.IsValid)
@@ -85,7 +129,18 @@ namespace BookService.Controllers
             db.Books.Add(book);
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+            // New code:
+            // Load author name
+            db.Entry(book).Reference(x => x.Author).Load();
+
+            var dto = new BookDTO()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name
+            };
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, dto);
         }
 
         // DELETE api/Books/5
