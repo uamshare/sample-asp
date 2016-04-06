814f96c2d69707b5c2a23e99f769704ae6963a45
utiuam <utiuam@gmail.com>
2016-04-06 09:23:51 +0700

    Display Item Details

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -24,6 +24,14 @@
         });
     }
 
+    self.detail = ko.observable();
+
+    self.getBookDetail = function (item) {
+        ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
+
     // Fetch the initial data.
     getAllBooks();
 };
